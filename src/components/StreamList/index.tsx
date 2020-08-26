d07f740b815c79310efad7c27b05502d5367bfda
hiagopsilva <hiagohps360@gmail.com>
2020-08-26 15:30:38 -0300

    add StreamList component

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+import { 
+  List, 
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles';
+
+const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numbeOfLines={1}>rodz_oficial</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numbeOfLines={1}>
+            Front-end com Next.js, Chakra UI e GraphQL.
+          </StreamDescription>
+
+          <StreamCategory numbeOfLines={1}>
+            Science & Technology
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  )
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
